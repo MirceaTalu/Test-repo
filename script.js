a911b5d6f51fb07f1e840e96788fe3009e8bb47f
MirceaTalu <talu.st.mircea@student.utcluj.ro>
2023-09-09 15:37:51 +0300

    Add Maven project

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Hello World!");
+
+// Path: script.js
+
+const button = document.getElementById("button");
+
+button.addEventListener("click", () => {
+  console.log("Button clicked");
+});
